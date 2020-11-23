126e210b9a131d3858d680cc431c15198416dd8f
My name IS <st.xsar@gmail.com>
2020-11-23 14:37:07 +0300

    Initial commit

---
+++ app/src/main/java/com/insanesun/crazy2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.insanesun.crazy2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView:TextView = findViewById(R.id.first_activity_text_view);
+        textView.setOnClickListener { moveToNextScreen() }
+    }
+
+    private fun moveToNextScreen() {
+        val intent: Intent = Intent(this,MovieDetailsActivity::class.java)
+        startActivity(intent)
+    }
+}
